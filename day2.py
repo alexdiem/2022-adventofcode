ba8cb9f8e2d8b765a92c111a2e0036580046698e
Alexandra Diem <alexandra.diem@gjensidige.no>
2022-12-02 15:24:28 +0100

    day 2 part 2

--- day2.py
+++ day2.py
@@ -20,7 +20,25 @@ def partone():
 
 
 def parttwo():
-    return None
+    STRATEGY = {'X': 0, 'Y': 3, 'Z': 6}
+    WIN = {'A': 'B', 'B': 'C', 'C': 'A'}
+    LOOSE = {'A': 'C', 'B': 'A', 'C': 'B'}
+    PTS_PLAY = {'A': 1, 'B': 2, 'C': 3}
+
+    points = 0
+    with open('data/day2') as f:
+        for line in f.readlines():
+            play = line.split()
+            points += STRATEGY[play[1]]
+
+            if STRATEGY[play[1]] == 0:
+                points += PTS_PLAY[LOOSE[play[0]]]
+            elif STRATEGY[play[1]] == 3:
+                points += PTS_PLAY[play[0]]
+            else:
+                points += PTS_PLAY[WIN[play[0]]]
+
+    return points
 
 
 if __name__ == "__main__":
